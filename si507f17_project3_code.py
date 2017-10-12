0c0a42a7a70a148bb8937396ab7b44646cb60740
Stella Choi <chsweet@umich.edu>
2017-10-12 01:20:15 -0400

    add scraping parks information from arkansas, california, michigan

--- si507f17_project3_code.py
+++ si507f17_project3_code.py
@@ -1,6 +1,9 @@
 from bs4 import BeautifulSoup
 import unittest
 import requests
+import logging, sys
+import re
+from urllib.parse import urljoin
 
 #########
 ## Instr note: the outline comments will stay as suggestions, otherwise it's too difficult.
@@ -9,10 +12,25 @@ import requests
 ## NOTE OF ADVICE:
 ## When you go to make your GitHub milestones, think pretty seriously about all the different parts and their requirements, and what you need to understand. Make sure you've asked your questions about Part 2 as much as you need to before Fall Break!
 
+logging.basicConfig(stream=sys.stderr, level=logging.DEBUG)
 
 ######### PART 0 #########
 
 # Write your code for Part 0 here.
+def print_alt_txt(website):
+    data = requests.get(website).text
+    soup = BeautifulSoup(data, 'html.parser')
+    # logging.debug(soup)
+    imgs = soup.findAll('img')
+    # logging.debug(imgs)
+    for img in imgs:
+        if img.has_attr('alt'):
+            print(img['alt'])
+        else:
+            print("No alternative text provided!")
+
+
+print_alt_txt("http://newmantaylor.com/gallery.html")
 
 
 ######### PART 1 #########
@@ -26,9 +44,6 @@ import requests
 # that the rest of the program can access.
 
 # We've provided comments to guide you through the complex try/except, but if you prefer to build up the code to do this scraping and caching yourself, that is OK.
-
-
-
 
 
 
@@ -69,9 +84,38 @@ import requests
 # And then, write each set of data to a file so this won't have to run again.
 
 
+def cache_site(web_site, cache_file):
+    try:
+        data = open(cache_file, 'r').read()
+    except:
+        data = requests.get(web_site).text
+        file = open(cache_file, 'w')
+        file.write(main_data)
+        file.close()
+    return data
 
 
+main_page = "https://www.nps.gov/index.htm"
+main_data = cache_site(main_page, "nps_gov_data.html")
+soup = BeautifulSoup(main_data, 'html.parser')
 
+search_ul = soup.find('ul', attrs={'class': "dropdown-menu SearchBar-keywordSearch"})
+search_lis = search_ul.find_all('li')
+# logging.debug(search_lis)
+
+states = ["Arkansas", "California", "Michigan"]
+links = {}
+for li in search_lis:
+    search_a = li.find('a')
+    if search_a.text in states:
+        link = search_a['href']
+        full_link = urljoin(main_page, link)
+        # logging.debug(full_link)
+        links[search_a.text] = full_link
+
+ar_data = cache_site(links["Arkansas"], "arkansas_data.html")
+ca_data = cache_site(links["California"], "california_data.html")
+mi_data = cache_site(links["Michigan"], "michigan_data.html")
 
 
 ######### PART 2 #########
@@ -79,35 +123,122 @@ import requests
 ## Before truly embarking on Part 2, we recommend you do a few things:
 
 # - Create BeautifulSoup objects out of all the data you have access to in variables from Part 1
-# - Do some investigation on those BeautifulSoup objects. What data do you have about each state? How is it organized in HTML?
+# - Do some investigation on those BeautifulSoup objects. What data do you have about each state?
+#  How is it organized in HTML?
 
-# HINT: remember the method .prettify() on a BeautifulSoup object -- might be useful for your investigation! So, of course, might be .find or .find_all, etc...
+ar_soup = BeautifulSoup(ar_data, "html.parser")
+ca_soup = BeautifulSoup(ca_data, "html.parser")
+mi_soup = BeautifulSoup(mi_data, "html.parser")
 
-# HINT: Remember that the data you saved is data that includes ALL of the parks/sites/etc in a certain state, but you want the class to represent just ONE park/site/monument/lakeshore.
+# HINT: remember the method .prettify() on a BeautifulSoup object -- might be useful for your investigation!
+#  So, of course, might be .find or .find_all, etc...
 
-# We have provided, in sample_html_of_park.html an HTML file that represents the HTML about 1 park. However, your code should rely upon HTML data about Michigan, Arkansas, and Califoria you saved and accessed in Part 1.
+#logging.debug(ar_soup.prettify())
+#logging.debug(ca_soup.prettify())
+#logging.debug(mi_soup.prettify())
 
-# However, to begin your investigation and begin to plan your class definition, you may want to open this file and create a BeautifulSoup instance of it to do investigation on.
+# HINT: Remember that the data you saved is data that includes ALL of the parks/sites/etc in a certain state,
+#  but you want the class to represent just ONE park/site/monument/lakeshore.
 
-# Remember that there are things you'll have to be careful about listed in the instructions -- e.g. if no type of park/site/monument is listed in input, one of your instance variables should have a None value...
+# We have provided, in sample_html_of_park.html an HTML file that represents the HTML about 1 park.
+#  However, your code should rely upon HTML data about Michigan, Arkansas, and Califoria you saved and accessed in Part 1.
 
+# However, to begin your investigation and begin to plan your class definition,
+#  you may want to open this file and create a BeautifulSoup instance of it to do investigation on.
 
+# Remember that there are things you'll have to be careful about listed in the instructions
+#  -- e.g. if no type of park/site/monument is listed in input, one of your instance variables should have a None value...
 
 
 
 ## Define your class NationalSite here:
 
 
+class NationalSite(object):
+
+    # precond: soup is li object under ul with id "list-parks"
+    def __init__(self, soup):
+        self.location = soup.find('h4').text
+        self.name = soup.find('a').text
+        logging.debug(self.name)
+        # find park type in h2
+        self.type = None
+        if soup.find('h2'):
+            self.type = soup.find('h2').text
+
+        # find description in p
+        self.description = ""
+        if soup.find('p'):
+            self.description = soup.find('p').text.strip()
+
+        # logging.debug(self.description)
+
+        # find basic info link which has address info
+        links = soup.find_all('a')
+        self.basic_info = None
+        for link in links:
+            if "Basic Information" in link.text.strip():
+                self.basic_info = link
+        # logging.debug(self.basic_info.text)
+
+    def __str__(self):
+        return self.name + " | " + self.location
+
+    def get_mailing_address(self):
+        link = self.basic_info['href']
+        logging.debug(link)
+        info = requests.get(link).text
+        info_soup = BeautifulSoup(info, 'html.parser')
+        try:
+            info_div = info_soup.find(id="BasicInfoAccordion")
+            addr_div = info_div.find('div', attrs={'class': "physical-address"})
+        except:
+            addr_div = info_soup.find('div', attrs={'class': "mailing-address"})
+
+        addr_elements = []
+        if addr_div:
+            addr_elements = addr_div.text.strip().split("\n")
+            logging.debug(addr_elements)
+
+        address = ""
+        for element in addr_elements:
+            # leading and trailing characters removed
+            element = element.strip()
+            if element:
+                address += element + " / "
+        if address:
+            address = address[0:-2]
+        logging.debug(address)
+        return address
+
+        #spans = addr_div.find_all('span')
+        #for span in spans:
+        #    if not span.find('span'):
+        #        address += span.text.strip().replace("\n", "") + "/"
+
+        #addr_div = info_soup.find('div', attrs={'class':"mailing-address"})
+        #loggin.debug(addr_div)
+        #logging.debug(address)
+        #return address
+        #spans = addr_div.find_all('span')
+        #address = ""
+        #for span in spans:
+        #    if not span['itemprop'].find('span'):
+        #        address += span.text.strip().replace("\n", "") + "/"
+
+    def __contains__(self, str):
+        return str in self.name
 
 
 
 ## Recommendation: to test the class, at various points, uncomment the following code and invoke some of the methods / check out the instance variables of the test instance saved in the variable sample_inst:
 
-# f = open("sample_html_of_park.html",'r')
-# soup_park_inst = BeautifulSoup(f.read(), 'html.parser') # an example of 1 BeautifulSoup instance to pass into your class
-# sample_inst = NationalSite(soup_park_inst)
-# f.close()
-
+f = open("sample_html_of_park.html",'r')
+soup_park_inst = BeautifulSoup(f.read(), 'html.parser') # an example of 1 BeautifulSoup instance to pass into your class
+sample_inst = NationalSite(soup_park_inst)
+sample_inst.get_mailing_address()
+f.close()
+print(sample_inst)
 
 ######### PART 3 #########
 
@@ -115,16 +246,35 @@ import requests
 
 # HINT: Get a Python list of all the HTML BeautifulSoup instances that represent each park, for each state.
 
+ar_park_soup = ar_soup.find(id="list_parks").find_all('li', attrs={'class': 'clearfix'})
+arkansas_natl_sites = []
+for park_soup in ar_park_soup:
+    park = NationalSite(park_soup)
+    arkansas_natl_sites.append(park)
+
+ca_park_soup = ca_soup.find(id="list_parks").find_all('li', attrs={'class': 'clearfix'})
+california_natl_sites = []
+# logging.debug(ca_park_soup)
+for park_soup in ca_park_soup:
+    park = NationalSite(park_soup)
+    california_natl_sites.append(park)
+
+mi_park_soup = mi_soup.find(id="list_parks").find_all('li', attrs={'class': 'clearfix'})
+michigan_natl_sites = []
+# logging.debug(mi_park_soup)
+for park_soup in mi_park_soup:
+    park = NationalSite(park_soup)
+    michigan_natl_sites.append(park)
 
 
 
 ##Code to help you test these out:
-# for p in california_natl_sites:
-# 	print(p)
-# for a in arkansas_natl_sites:
-# 	print(a)
-# for m in michigan_natl_sites:
-# 	print(m)
+for p in california_natl_sites:
+ 	print(p)
+for a in arkansas_natl_sites:
+ 	print(a)
+for m in michigan_natl_sites:
+ 	print(m)
 
 
 
@@ -136,3 +286,18 @@ import requests
 
 ## Also remember that IF you have None values that may occur, you might run into some problems and have to debug for where you need to put in some None value / error handling!
 
+
+def create_csv_file(file_name, list):
+    with open(file_name, 'w') as csv_file:
+        csv_file.write('Name, Location, Type, Address, Description\n')
+        for piece in list:
+            type = piece.type
+            if not type:
+                type = "None"
+            csv_file.write('\"{}\", \"{}\", {}, \"{}\", \"{}\"\n'.format(piece.name, piece.location,
+                           type, piece.get_mailing_address(), piece.description))
+
+
+create_csv_file('arkansas.csv', arkansas_natl_sites)
+create_csv_file('california.csv', california_natl_sites)
+create_csv_file('michigan.csv', michigan_natl_sites)
